9a483e3804ad332244377ed648d4496e6bf66271
Steve Winton <stevewinton@gmail.com>
2012-09-20 15:52:13 +0100

    Using Tweepy for userstream consumption

--- mopidyextra/frontends/twitter/base.py
+++ mopidyextra/frontends/twitter/base.py
@@ -1,15 +1,16 @@
 import logging
-
+import httplib
+import json
+import ssl
 import threading
+import time
+
+import tweepy
 
 from pykka.actor import ThreadingActor
 
 from mopidy import settings, SettingsError
 from mopidy.listeners import BackendListener
-
-import requests
-
-from oauth_hook import OAuthHook
 
 import mopidyextra.utils as utils
 
@@ -24,11 +25,9 @@ class TwitterFrontend(ThreadingActor, BackendListener):
         self.session = TwitterSession(frontend_ref=self.actor_ref)
 
     def on_start(self):
-        logger.info(u'Connecting to Twitter')
         self.session.start()
 
     def on_stop(self):
-        logging.info(u'Disconnecting from Twitter')
         self.session.stop()
 
     def on_receive(self, message):
@@ -53,7 +52,7 @@ class TwitterFrontend(ThreadingActor, BackendListener):
         
         return
 
-class TwitterSession(object):
+class TwitterSession(tweepy.StreamListener):
     """
     Connects to the user stream, passes incoming data from Twitter off to the dispatcher.
 
@@ -61,6 +60,8 @@ class TwitterSession(object):
     """
     
     def __init__(self, frontend_ref):
+        super(tweepy.StreamListener, self).__init__()
+
         self.frontend_ref = frontend_ref
         self.dispatcher = TwitterDispatcher(session=self)
 
@@ -72,25 +73,58 @@ class TwitterSession(object):
         self._consumer = threading.Thread(target=self.consume)
         self._stop = threading.Event()
 
+        self._stream = None
+
     def consume(self):
-        userstream_generator = utils.userstream(self.access_token, self.access_token_secret, 
-                                                self.consumer_key, self.consumer_secret)
+        logger.info(u'Connecting to Twitter')
+
+        # Create an auth handler
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        
+        # Connect to stream
+        self._stream = tweepy.Stream(auth, self, secure=True)
 
         while not self._stop.is_set():
-            obj = userstream_generator.next()
-            logger.debug(u'Received %s from stream (stopped: %s)' % (repr(obj), repr(self._stop.is_set())))
-            if obj is not None:
-                try:
-                    for response in self.dispatcher.handle_request(obj):
-                        self.frontend_ref.tell(response)
-                except ValueError:
-                    continue
+
+            try:
+                self._stream.userstream()
+
+            except httplib.IncompleteRead, e:
+                logging.debug(u'Exception: %s' % str(e))
+                time.sleep(5)
+
+            except ssl.SSLError, e:
+                logging.debug(u'Exception: %s' % str(e))
+                time.sleep(5)
+
+            except Exception, e:
+                logging.debug(u'Unexpected exception: %s' % str(e)) 
+                time.sleep(5)
 
     def start(self):
         self._consumer.start()
 
     def stop(self):
-        logger.info(u'Stopping, please wait...')
+        logging.info(u'Disconnecting from Twitter... ')
         self._stop.set()
+        self._stream.disconnect()
         self._consumer.join()
+        logging.info(u'Disconnected from Twitter.')
 
+    def on_data(self, data):
+        if not data.strip():
+            return True
+        
+        # Decode JSON data
+        obj = json.loads(data)
+        logging.debug(u'Received: %s from twitter' % repr(obj))
+
+        if obj is not None:
+            try:
+                for response in self.dispatcher.handle_request(obj):
+                    self.frontend_ref.tell(response)
+            except ValueError:
+                pass
+
+        return True
